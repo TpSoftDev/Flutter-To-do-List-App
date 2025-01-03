d9897601f4eb95cd14e1a9ffd0fca2abbc1a5409
Thabang <tp.softwaredev@gmail.com>
2025-01-02 23:34:01 -0600

    Working Navigation Bar

--- to_do_list_app/lib/pages/landing.dart
+++ to_do_list_app/lib/pages/landing.dart
@@ -3,11 +3,16 @@ import 'package:to_do_list_app/pages/home.dart';
 import 'package:to_do_list_app/pages/profile.dart';
 import 'package:to_do_list_app/pages/settings.dart';
 
-class LandingPage extends StatelessWidget {
+class LandingPage extends StatefulWidget {
   LandingPage({super.key});
 
-  // variable to store the index of the page
-  int index = 0;
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  // variable to store the selectedIndex of the page
+  int selectedIndex = 0;
 
   // Create a list of pages
   final List<Widget> pages = [
@@ -16,9 +21,11 @@ class LandingPage extends StatelessWidget {
     SettingsPage(),
   ];
 
-  // function to update the index of the page
+  // function to update the selectedIndex of the page
   void navigateBottomBar(int index) {
     setState(() {
+      selectedIndex = index;
+    });
   }
 
   @override
@@ -29,8 +36,10 @@ class LandingPage extends StatelessWidget {
           "Landing Page",
         ),
       ),
-      body: pages[index],
+      body: pages[selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: navigateBottomBar,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
